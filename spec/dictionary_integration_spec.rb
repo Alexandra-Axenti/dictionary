0ac8625ecc1fb1f6cce4e444417a0755ca955952
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-31 11:05:30 +0800

    add 2 more integration tests

--- spec/dictionary_integration_spec.rb
+++ spec/dictionary_integration_spec.rb
@@ -4,6 +4,18 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the dictionary verification path', {:type => :feature}) do
+  it('redirects the user to the form that adds new entries to the dictionary') do
+    visit('/')
+    click_button('Add New Word')
+    expect(page).to have_content("Please add a new word and add a definition to it:")
+  end
+
+  it("redirects the user to display page that lists all the dictionary's entries") do
+    visit('/')
+    click_button('See All Words')
+    expect(page).to have_content(@@words)
+  end
+
   it('processes the user input and displays a new word') do
     visit('/words/new')
     fill_in('word', :with => 'jenga code')
