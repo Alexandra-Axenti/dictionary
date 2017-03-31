ec1572db5601ba8f85b4e0364ba602409cec80c2
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-31 10:37:41 +0800

    add the Word class

--- lib/word.rb
+++ lib/word.rb
@@ -1,3 +1,47 @@
 class Word
+  @@words = []
 
+  define_method(:initialize) do |word|
+    @word = word
+    @id = @@words.length()
+    @definitions = []
+  end
+
+  define_method(:word) do
+    @word
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_method(:definitions) do
+    @definitions
+  end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@words = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_word = nil
+    @@words.each() do |word|
+      if word.id().eql?(id)
+        found_word = word
+      end
+    end
+    found_word
+  end
+
+  define_method(:add_definition) do |definition|
+    @definitions.push(definition)
+  end
 end
