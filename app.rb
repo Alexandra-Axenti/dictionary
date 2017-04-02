e8e15b63ca0c7ee95c846eb11e8fc280a2a44ec5
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-04-02 12:39:53 +0800

    add some styles and correct some views pages

--- app.rb
+++ app.rb
@@ -12,6 +12,20 @@ get('/words/new') do
   erb(:form)
 end
 
+get('/words/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:word)
+end
+
+post('/words/:id') do
+  definition = params.fetch("definition")
+  @definition = Definition.new(definition)
+  @word = Word.find(params.fetch('id').to_i())
+  @word.add_definition(@definition)
+  @word.save()
+  erb(:word)
+end
+
 get('/words') do
   @words = Word.all()
   erb(:words)
